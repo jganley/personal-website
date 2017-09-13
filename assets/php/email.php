81f79f3c05faff3d88f1d21390e3f4bc6a7c8c51
jganley <jganley@ucla.edu>
2017-09-12 20:56:25 -0700

    fixed bug with php file

--- assets/php/email.php
+++ assets/php/email.php
@@ -68,7 +68,7 @@ $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
-header('Location: sentmail.html');
+header('Location: /sentmail.html');
 
 }
 ?>
